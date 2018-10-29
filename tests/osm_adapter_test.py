d12b73a39d679eb4623ee61efa5550a7e7d7e3f9
Dharmin <dharmingeo@gmail.com>
2018-10-29 21:13:09 +0100

    adapter working with multiple ids

--- tests/osm_adapter_test.py
+++ tests/osm_adapter_test.py
@@ -4,7 +4,7 @@ import unittest
 class TestOSMAdapter(unittest.TestCase):
 
     def setUp(self):
-        self.osm_adapter = OSMAdapter()
+        self.osm_adapter = OSMAdapter(debug=True)
 
     def test_get_osm_element_by_id(self): 
         result_node = self.osm_adapter.get_osm_element_by_id(ids=[4865],data_type='node')
@@ -19,6 +19,31 @@ class TestOSMAdapter(unittest.TestCase):
         result_relation_role = self.osm_adapter.get_osm_element_by_id(ids=[149],data_type='relation',role='geometry',role_type='way')
         self.assertEqual(len(result_relation_role[1]), 1)
 
+    def test_get_osm_element_by_multiple_id(self): 
+        nodes,__,__ = self.osm_adapter.get_osm_element_by_id(ids=[4866,4865,4864],data_type='node')
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].id, 4866)
+        self.assertEqual(nodes[1].id, 4865)
+        self.assertEqual(nodes[2].id, 4864)
+
+        __,way,__ = self.osm_adapter.get_osm_element_by_id(ids=[500,501,499],data_type='way')
+        self.assertEqual(len(way), 3)
+        self.assertEqual(way[0].id, 500)
+        self.assertEqual(way[1].id, 501)
+        self.assertEqual(way[2].id, 499)
+
+        __,__,relation = self.osm_adapter.get_osm_element_by_id(ids=[149,151,150],data_type='relation')
+        self.assertEqual(len(relation), 3)
+        self.assertEqual(relation[0].id, 149)
+        self.assertEqual(relation[1].id, 151)
+        self.assertEqual(relation[2].id, 150)
+
+        __,way,__ = self.osm_adapter.get_osm_element_by_id(ids=[150,151,149],data_type='relation',role='geometry',role_type='way')
+        self.assertEqual(len(way), 3)
+        self.assertEqual(way[0].id, 1309)
+        self.assertEqual(way[1].id, 1312)
+        self.assertEqual(way[2].id, 1352)
+        
     def test_search_by_tag(self):
         result_node = self.osm_adapter.search_by_tag(data_type='node',key='highway',value='elevator')
         self.assertTrue(len(result_node[0]) >  0)
