8decfb81b3a4049c0f740c9b029ed38a96339661
Dharmin <dharmingeo@gmail.com>
2018-11-07 15:19:28 +0100

    solves issue 17

--- OBL/occ_grid_generator.py
+++ OBL/occ_grid_generator.py
@@ -19,8 +19,7 @@ class OccGridGenerator(object):
     _scale = 1
 
     def __init__(self, osm_bridge, *args, **kwargs):
-        """TODO: to be defined1. 
-        
+        """ 
         args
         :osm_bridge: OSMBridge object
         :debug: boolean
@@ -63,7 +62,7 @@ class OccGridGenerator(object):
             except OSError as exc: # Guard against race condition
                 print(str(exc))
 
-
+        print("Trying to create map file...")
         grid_map = Image.new('L', (self._dimension*self._scale,self._dimension*self._scale),255) #128
 
         drawObject = ImageDraw.Draw(grid_map)
@@ -71,11 +70,36 @@ class OccGridGenerator(object):
 #        ways = []
         ways.extend(self._get_elevator_walls())
         ways.extend(self._get_doors(floor))
+        max_x , max_y = 0, 0
         for way in ways:
             way_cartesian = [self.osm_bridge.convert_to_cartesian(n.lat, n.lon) for n in way]
+            local_max_x = max(way_cartesian, key=lambda x: x[0])[0]
+            local_max_y = max(way_cartesian, key=lambda y: y[1])[1]
+            max_x = local_max_x if local_max_x > max_x else max_x
+            max_y = local_max_y if local_max_y > max_y else max_y
             drawObject.polygon(way_cartesian,0,0)
-        grid_map.save(self._dirname + "/" + self._file_name + "_floor_" + str(floor) + ".pgm")
+        self.logger.debug("size x " + str(max_x))
+        self.logger.debug("size y " + str(max_y))
+        cropped_image = grid_map.crop((0,0, int(max_x+100), int(max_y+100)))
+        cropped_image.save(self._dirname + "/" + self._file_name + "_floor_" + str(floor) + ".pgm")
         self._save_yaml_file(floor)
+        print("Map files saved at " + os.path.abspath(self._dirname))
+
+    def generate_map_all_floor(self, building=None):
+        """generate map of all the floor in the given building
+
+        :building: String
+        :returns: None
+
+        """
+        if building == None :
+            raise Exception("Building ref is required")
+        building_object = self.osm_bridge.get_building(building)
+        floors = building_object.floors
+        for floor in floors :
+            self.logger.debug(floor.ref)
+            floor_number = int(floor.ref.split("_")[1][1:])
+            self.generate_map(floor=floor_number)
 
     def _get_walls(self, floor):
         """returns all the nodes for all ways with wall tag
@@ -129,23 +153,21 @@ class OccGridGenerator(object):
         :returns: None
 
         """
-        grid_map_origin = [0,0]
-        grid_map_origin[1] = ((self._dimension*self.osm_bridge.resolution*self._scale) - self.osm_bridge.local_origin[0]) - (- self.osm_bridge.local_origin[1])
-        grid_map_origin[0] = self.osm_bridge.local_origin[1] - self.osm_bridge.local_origin[0]
+#        grid_map_origin = [0,0]
+#        grid_map_origin[0] = self.osm_bridge.local_origin[1] - self.osm_bridge.local_origin[0]
+#        grid_map_origin[1] = ((self._dimension*self.osm_bridge.resolution*self._scale) - self.osm_bridge.local_origin[0]) + self.osm_bridge.local_origin[1]
+
         data = dict(
             image = self._file_name + '_floor_' + str(floor) + '.pgm',
             resolution = self.osm_bridge.resolution*self._scale,
-            origin = [grid_map_origin[0], grid_map_origin[1], 0.0],
+#            origin = [grid_map_origin[0], grid_map_origin[1], 0.0],
+            origin = [self.osm_bridge.local_origin[0]*self._scale, self.osm_bridge.local_origin[1]*self._scale, 0.0],
             negate = 0,
             latitude = self.osm_bridge.global_origin[0],
             longitude = self.osm_bridge.global_origin[1],
-            occupied_thresh = 0.99,
-            free_thresh =  0.01
+            occupied_thresh = 0.9,
+            free_thresh =  0.1
         )
 
         with open(self._dirname + "/" + self._file_name + '_floor_' + str(floor) + '.yaml', 'w') as outfile:
             yaml.dump(data, outfile, default_flow_style=True)
-"""
-
-
-"""
--- tests/occ_grid_generator_test.py
+++ tests/occ_grid_generator_test.py
@@ -11,17 +11,27 @@ class TestOccGridGenerator(unittest.TestCase):
 
     def setUp(self):
         self.osm_bridge = OSMBridge()
-        self.occ_grid_generator = OccGridGenerator(self.osm_bridge, debug=True)
+        self.occ_grid_generator = OccGridGenerator(self.osm_bridge, debug=False)
 
     def test_map_files_exists(self):
         self.occ_grid_generator.generate_map(floor=-1)
         file_list = os.listdir("../")
-        print(file_list)
         for (dirpath, dirnames, filenames) in os.walk("../"):
             self.assertIn("maps", dirnames)
             break
         self.assertIn("map_floor_-1.pgm", os.listdir("../maps/"))
         self.assertIn("map_floor_-1.yaml", os.listdir("../maps/"))
+
+    def test_map_files_exists_multiple_floor(self):
+        self.occ_grid_generator.generate_map_all_floor(building="AMK")
+        file_list = os.listdir("../")
+        for (dirpath, dirnames, filenames) in os.walk("../"):
+            self.assertIn("maps", dirnames)
+            break
+        self.assertIn("map_floor_-1.pgm", os.listdir("../maps/"))
+        self.assertIn("map_floor_-1.yaml", os.listdir("../maps/"))
+        self.assertIn("map_floor_4.pgm", os.listdir("../maps/"))
+        self.assertIn("map_floor_4.yaml", os.listdir("../maps/"))
         
 
 if __name__ == '__main__':
