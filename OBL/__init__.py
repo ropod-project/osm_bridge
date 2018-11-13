b515fa86ed7fedd92006dd4e9985e35de08beb56
Dharmin <dharmingeo@gmail.com>
2018-11-13 21:40:56 +0100

    Moved local area finder in osm_bridge

--- OBL/__init__.py
+++ OBL/__init__.py
@@ -1,5 +1,4 @@
 from OBL.osm_bridge import OSMBridge
 from OBL.osm_adapter import OSMAdapter
 from OBL.path_planner import PathPlanner
-from OBL.local_area_finder import LocalAreaFinder
 from OBL.occ_grid_generator import OccGridGenerator
--- tests/local_area_finder_test.py
+++ tests/local_area_finder_test.py
@@ -1,5 +1,4 @@
 from OBL import OSMBridge
-from OBL import LocalAreaFinder
 
 import unittest
 
@@ -10,7 +9,6 @@ class TestLocalAreaFinder(unittest.TestCase):
 
     def setUp(self):
         self.osm_bridge = OSMBridge()
-        self.local_area_finder = LocalAreaFinder(self.osm_bridge, debug=False)
 
     def test_given_area_name_inside_la_with_xy(self):
         """TODO: Docstring for test_given_area_name_inside_la_with_xy.
@@ -20,7 +18,7 @@ class TestLocalAreaFinder(unittest.TestCase):
         expected_local_area_ref = "AMK_B_L-1_C29_LA1"
         x = 14.054641069320496
         y = -20.23611060809344
-        local_area = self.local_area_finder.get_local_area(x=x, y=y, area_name=area_name, isLatlong=False)
+        local_area = self.osm_bridge.get_local_area(x=x, y=y, area_name=area_name, isLatlong=False)
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
     def test_given_area_name_outside_la_with_xy(self):
@@ -31,7 +29,7 @@ class TestLocalAreaFinder(unittest.TestCase):
         expected_local_area_ref = "AMK_B_L-1_C29_LA1"
         x = 13.190239037096035
         y = -20.165316094644368
-        local_area = self.local_area_finder.get_local_area(x=x, y=y, area_name=area_name, isLatlong=False)
+        local_area = self.osm_bridge.get_local_area(x=x, y=y, area_name=area_name, isLatlong=False)
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
     def test_given_area_name_inside_la_with_latlong(self):
@@ -42,7 +40,7 @@ class TestLocalAreaFinder(unittest.TestCase):
         expected_local_area_ref = "AMK_B_L-1_C29_LA1"
         latitude = 50.1361671
         longitude = 8.6476004
-        local_area = self.local_area_finder.get_local_area(x=latitude, y=longitude, area_name=area_name, isLatlong=True)
+        local_area = self.osm_bridge.get_local_area(x=latitude, y=longitude, area_name=area_name, isLatlong=True)
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
     def test_given_area_name_outside_la_with_latlong(self):
@@ -53,7 +51,7 @@ class TestLocalAreaFinder(unittest.TestCase):
         expected_local_area_ref = "AMK_B_L-1_C29_LA1"
         latitude = 50.1361677
         longitude = 8.6475883
-        local_area = self.local_area_finder.get_local_area(x=latitude, y=longitude, area_name=area_name, isLatlong=True)
+        local_area = self.osm_bridge.get_local_area(x=latitude, y=longitude, area_name=area_name, isLatlong=True)
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
     def test_not_given_area_name(self):
@@ -63,7 +61,7 @@ class TestLocalAreaFinder(unittest.TestCase):
         expected_local_area_ref = "AMK_B_L-1_C29_LA1"
         latitude = 50.1361677
         longitude = 8.6475883
-        local_area = self.local_area_finder.get_local_area(x=latitude, y=longitude, isLatlong=True, floor_name="AMK_L-1")
+        local_area = self.osm_bridge.get_local_area(x=latitude, y=longitude, isLatlong=True, floor_name="AMK_L-1")
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
     def test_not_given_area_name_edge_case(self):
@@ -74,7 +72,7 @@ class TestLocalAreaFinder(unittest.TestCase):
         expected_local_area_ref = "AMK_B_L-1_C23_LA2"
         latitude = 50.1363136
         longitude = 8.6475123
-        local_area = self.local_area_finder.get_local_area(x=latitude, y=longitude, isLatlong=True, floor_name="AMK_L-1")
+        local_area = self.osm_bridge.get_local_area(x=latitude, y=longitude, isLatlong=True, floor_name="AMK_L-1")
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
     def test_given_area_name_with_behaviour(self):
@@ -84,12 +82,12 @@ class TestLocalAreaFinder(unittest.TestCase):
         """
         area_name = "AMK_D_L-1_C41"
         expected_local_area_ref = "AMK_D_L-1_C41_LA2"
-        local_area = self.local_area_finder.get_local_area(area_name=area_name, behaviour="docking")
+        local_area = self.osm_bridge.get_local_area(area_name=area_name, behaviour="docking")
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
         area_name = "AMK_A_L-1_RoomBU21"
         expected_local_area_ref = "AMK_A_L-1_RoomBU21_LA1"
-        local_area = self.local_area_finder.get_local_area(area_name=area_name, behaviour="undocking")
+        local_area = self.osm_bridge.get_local_area(area_name=area_name, behaviour="undocking")
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
     def test_not_given_area_name_with_behaviour(self):
@@ -99,12 +97,12 @@ class TestLocalAreaFinder(unittest.TestCase):
         """
         floor_name = "AMK_L-1"
         expected_local_area_ref = "AMK_D_L-1_C41_LA2"
-        local_area = self.local_area_finder.get_local_area(floor_name=floor_name, behaviour="docking")
+        local_area = self.osm_bridge.get_local_area(floor_name=floor_name, behaviour="docking")
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
         floor_name = "AMK_L-1"
         expected_local_area_ref = "AMK_A_L-1_RoomBU21_LA1"
-        local_area = self.local_area_finder.get_local_area(floor_name=floor_name, behaviour="undocking")
+        local_area = self.osm_bridge.get_local_area(floor_name=floor_name, behaviour="undocking")
         self.assertEqual(local_area.ref, expected_local_area_ref)
 
 if __name__ == '__main__':
