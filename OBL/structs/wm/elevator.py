780e0213fe710715a377aa5c79e8ae298c145eea
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-11-14 19:01:11 +0100

    Changed all calls to superclass constructors so that they're also compatible with Python 2

--- OBL/structs/wm/elevator.py
+++ OBL/structs/wm/elevator.py
@@ -2,6 +2,5 @@ from OBL.structs.wm.area import Area
 
 class Elevator(Area):
 
-    def __init__(self, elevator_ref, *args, **kwargs):      
-        super().__init__(elevator_ref, *args, **kwargs)
-
+    def __init__(self, elevator_ref, *args, **kwargs):
+        super(Elevator, self).__init__(elevator_ref, *args, **kwargs)
