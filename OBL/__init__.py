7dfa1ae8ef3dc93ab624c3b620951de9465962a8
Lakshadeep Naik <lakshadeep.naik@gmail.com>
2018-11-29 15:13:57 +0100

    Fixed import error

--- OBL/__init__.py
+++ OBL/__init__.py
@@ -2,3 +2,4 @@ from OBL.osm_bridge import OSMBridge
 from OBL.osm_adapter import OSMAdapter
 from OBL.path_planner import PathPlanner
 from OBL.occ_grid_generator import OccGridGenerator
+from OBL.structs import *
